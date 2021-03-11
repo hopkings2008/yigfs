c46d008b636fde6c04e134996ebb4482ed08f9ba
YuZou <hopkings2005@hotmail.com>
2021-03-11 15:48:00 +0800

    integrate leader_mgr into segment_mgr.

--- client/segment_mgr/src/segment_mgr.rs
+++ client/segment_mgr/src/segment_mgr.rs
@@ -1,17 +1,15 @@
 extern crate tokio;
 
-use tokio::sync::mpsc;
-use crate::types::{Segment, Block};
+use crate::types::{Block, Segment, SegmentIo};
+use crate::leader_mgr::LeaderMgr;
 use common::error::Errno;
 use common::runtime::Executor;
 use metaservice_mgr::mgr::MetaServiceMgr;
-use io_engine::io_thread_pool::IoThreadPool;
-use io_engine::types::{MsgFileOpenOp, MsgFileOp, MsgFileWriteOp, MsgFileWriteResp};
+
 pub struct SegmentMgr<'a> {
     meta_service_mgr: &'a Box<dyn MetaServiceMgr>,
     data_dirs: Vec<String>,
-    io_pool: IoThreadPool,
-    exec: Executor,
+    leader_mgr: LeaderMgr,
 }
 
 impl<'a> SegmentMgr<'a> {
@@ -57,75 +55,52 @@ impl<'a> SegmentMgr<'a> {
     }
 
     pub fn open_segment(&self, seg: &Segment) -> Errno {
-        let worker = self.io_pool.get_worker(seg.seg_id0, seg.seg_id1);
-        let (tx, mut rx) = mpsc::channel::<Errno>(1);
-        let msg = MsgFileOpenOp{
+        let leader = self.leader_mgr.get_leader(&seg.leader);
+        let seg_io = SegmentIo{
             id0: seg.seg_id0,
             id1: seg.seg_id1,
             dir: self.data_dirs[0].clone(),
-            resp_sender: tx,
         };
-        let ret = worker.send_disk_io(MsgFileOp::OpOpen(msg));
-        if !ret.is_success() {
-            println!("open_segment(id0: {}, id1: {}): failed to send open msg, err: {:?}",
-            seg.seg_id0, seg.seg_id1, ret);
-            return ret;
-        }
-        let ret = self.exec.get_runtime().block_on(rx.recv());
-        if let Some(e) = ret {
-            return e;
-        }
-        println!("open_segment(id0: {}, id1: {}): got invalid ret", seg.seg_id0, seg.seg_id1);
-        return Errno::Eintr;
+        leader.open(&seg_io)
     }
 
     pub fn write_segment(&self, seg: &mut Segment, ino: u64, offset: u64, data: &[u8]) -> Result<u32, Errno> {
-        let worker = self.io_pool.get_worker(seg.seg_id0, seg.seg_id1);
-        let (tx, mut rx) = mpsc::channel::<MsgFileWriteResp>(1);
-        let msg = MsgFileWriteOp{
+        let seg_io = SegmentIo{
             id0: seg.seg_id0,
             id1: seg.seg_id1,
-            offset: offset,
-            data: data.to_vec(),
-            resp_sender: tx,
+            dir: self.data_dirs[0].clone(),
         };
-        let ret = worker.send_disk_io(MsgFileOp::OpWrite(msg));
-        if !ret.is_success() {
-            println!("write_segment: failed to send_disk_io for seg({:?}), offset: {}, err: {:?}",
-            seg, offset, ret);
-            return Err(Errno::Eintr);
-        }
-        let ret = self.exec.get_runtime().block_on(rx.recv());
-        if let Some(r) = ret {
-            if !r.err.is_success() {
-                println!("write_segment: failed to write segment for seg({:?}), offset: {}, err: {:?}",
-                seg, offset, r.err);
-                return Err(r.err);
+        let leader = self.leader_mgr.get_leader(&seg.leader);
+        let ret = leader.write(&seg_io, data);
+        match ret {
+            Ok(ret) => {
+                let nwrite = ret.size;
+                seg.add_block(ino, offset, ret.offset, nwrite);
+                // update the meta service.
+                let meta_seg = seg.to_meta_segment();
+                let ret = self.meta_service_mgr.add_file_block(ino, &meta_seg);
+                if !ret.is_success(){
+                    println!("failed to add file block for ino: {}, meta_seg: {:?}, err: {:?}",
+                    ino, meta_seg, ret);
+                    return Err(ret);
+                }
+                println!("write_segment: succeed to add_file_block for ino: {}, set: {:?},  nwrite: {}", 
+                    ino, meta_seg, nwrite);
+                return Ok(nwrite);
             }
-            // add block into segment.
-            seg.add_block(ino, offset, r.offset, r.nwrite);
-            // update the meta service.
-            let meta_seg = seg.to_meta_segment();
-            let ret = self.meta_service_mgr.add_file_block(ino, &meta_seg);
-            if !ret.is_success(){
-                println!("failed to add file block for ino: {}, meta_seg: {:?}, err: {:?}",
-                ino, meta_seg, ret);
-                return Err(ret);
+            Err(err) => {
+                println!("write_segment: failed to write({:?}) for ino: {}, offset: {}",
+                seg_io, ino, offset);
+                return Err(err);
             }
-            println!("write_segment: succeed to add_file_block for ino: {}, set: {:?},  nwrite: {}", 
-                ino, meta_seg, r.nwrite);
-            return Ok(r.nwrite);
         }
-        println!("write_segment: got invalid response for seg({:?}, offset: {}", seg, offset);
-        return Err(Errno::Eintr);
     }
 
     pub fn create(dirs: Vec<String>, mgr: &'a Box<dyn MetaServiceMgr>, exec: &Executor)->Box<SegmentMgr<'a>> {
         Box::new(SegmentMgr{
             meta_service_mgr: mgr,
             data_dirs: dirs,
-            io_pool: IoThreadPool::new(2, exec),
-            exec: exec.clone(),
+            leader_mgr: LeaderMgr::new(mgr.get_machine_id(), 2, exec),
         })
     }
 }
