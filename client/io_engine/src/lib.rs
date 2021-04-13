0b7c9cbf7d6ba064579c172075832cabec5b410f
YuZou <hopkings2005@hotmail.com>
2021-04-13 15:30:01 +0800

    switch to use the sync io in disk io thread.

--- client/io_engine/src/lib.rs
+++ client/io_engine/src/lib.rs
@@ -2,6 +2,5 @@ pub mod types;
 pub mod io_worker;
 pub mod io_thread;
 pub mod io_thread_pool;
-pub mod disk_io_thread;
-pub mod disk_io_thread_pool;
+pub mod disk_io_worker;
 mod file_handle_ref;
--- client/segment_mgr/src/leader_local.rs
+++ client/segment_mgr/src/leader_local.rs
@@ -1,9 +1,11 @@
 use std::rc::Rc;
-use tokio::sync::mpsc;
+use crossbeam_channel::bounded;
 use common::runtime::Executor;
 use common::error::Errno;
-use io_engine::{disk_io_thread_pool::DiskIoThreadPool, types::{MsgFileCloseOp, MsgFileReadData, MsgFileReadOp}};
-use io_engine::types::{MsgFileOpenOp, MsgFileOp, MsgFileWriteOp, MsgFileWriteResp};
+use io_engine::types::{MsgFileOpenOp, MsgFileReadOp, MsgFileOp, MsgFileWriteOp, 
+    MsgFileWriteResp, MsgFileCloseOp, MsgFileReadData};
+use io_engine::io_thread_pool::IoThreadPool;
+use io_engine::disk_io_worker::DiskIoWorkerFactory;
 use crate::leader::Leader;
 use crate::file_handle::FileHandleMgr;
 use crate::types::{FileHandle, Block, BlockIo, Segment};
@@ -11,7 +13,7 @@ use crate::segment_mgr::SegmentMgr;
 
 pub struct LeaderLocal {
     machine: String,
-    io_pool: DiskIoThreadPool,
+    disk_io_pool: IoThreadPool,
     exec: Executor,
     segment_mgr: Rc<SegmentMgr>,
     handle_mgr: FileHandleMgr,
@@ -45,30 +47,35 @@ impl Leader for LeaderLocal {
         }
         for seg in &segments {
             let seg_dir = self.segment_mgr.get_segment_dir(seg.seg_id0, seg.seg_id1);
-            let worker = self.io_pool.get_worker(seg.seg_id0, seg.seg_id1);
-            let (tx, mut rx) = mpsc::channel::<Errno>(1);
+            let worker = self.disk_io_pool.get_thread(seg.seg_id0, seg.seg_id1);
+            let (tx, rx) = bounded::<Errno>(1);
             let msg = MsgFileOpenOp{
                 id0: seg.seg_id0,
                 id1: seg.seg_id1,
                 dir: seg_dir,
                 resp_sender: tx,
             };
-            let ret = worker.send_disk_io(MsgFileOp::OpOpen(msg));
+            let ret = worker.do_io(MsgFileOp::OpOpen(msg));
             if !ret.is_success() {
                 println!("open(id0: {}, id1: {}): failed to send open msg, err: {:?}",
                 seg.seg_id0, seg.seg_id1, ret);
                 return ret;
             }
-            let ret = self.exec.get_runtime().block_on(rx.recv());
-            if let Some(e) = ret {
-                if !e.is_success() {
-                    println!("open(id0: {}, id1: {}) failed with errno: {:?}", seg.seg_id0, seg.seg_id1, e);
-                    return e;
+            let ret = rx.recv();
+            match ret {
+                Ok(e) => {
+                    if !e.is_success() {
+                        println!("open(id0: {}, id1: {}) failed with errno: {:?}", seg.seg_id0, seg.seg_id1, e);
+                        return e;
+                    }
+                    continue;
                 }
-                continue;
+                Err(err) => {
+                    println!("open: failed to get response for (id0: {}, id1: {}), err: {}", 
+                    seg.seg_id0, seg.seg_id1, err);
+                    return Errno::Eintr;
+                }
             }
-            println!("open(id0: {}, id1: {}): got invalid ret", seg.seg_id0, seg.seg_id1);
-            return Errno::Eintr;
         }
 
         let file_handle = FileHandle {
@@ -105,7 +112,7 @@ impl Leader for LeaderLocal {
                     }
                     // read the data.
                     let seg_dir = self.segment_mgr.get_segment_dir(s.seg_id0, s.seg_id1);
-                    let (tx, mut rx) = mpsc::channel::<MsgFileReadData>(1);
+                    let (tx, rx) = bounded::<MsgFileReadData>(1);
                     let msg = MsgFileReadOp{
                         id0: s.seg_id0,
                         id1: s.seg_id1,
@@ -114,15 +121,15 @@ impl Leader for LeaderLocal {
                         size: to_read,
                         data_sender: tx,
                     };
-                    let worker = self.io_pool.get_worker(s.seg_id0, s.seg_id1);
-                    let ret = worker.send_disk_io(MsgFileOp::OpRead(msg));
+                    let worker = self.disk_io_pool.get_thread(s.seg_id0, s.seg_id1);
+                    let ret = worker.do_io(MsgFileOp::OpRead(msg));
                     if !ret.is_success(){
                         println!("read: failed to read data for ino: {}, offset: {}, err: {:?}", ino, start, ret);
                         return Err(ret);
                     }
-                    let ret = self.exec.get_runtime().block_on(rx.recv());
+                    let ret = rx.recv();
                     match ret {
-                        Some(ret) => {
+                        Ok(ret) => {
                             if ret.err.is_success() {
                                 if let Some(d) = ret.data{
                                     let l = d.len() as u32;
@@ -139,9 +146,9 @@ impl Leader for LeaderLocal {
                                 return Err(ret.err);
                             }
                         }
-                        None => {
-                            println!("read: got invalid response for ino: {}, offset: {}, start: {}", 
-                            ino, offset, start);
+                        Err(err) => {
+                            println!("read: failed to got response for ino: {}, offset: {}, start: {}, err: {}", 
+                            ino, offset, start, err);
                         }
                     }
                     if total_read == 0 {
@@ -176,9 +183,9 @@ impl Leader for LeaderLocal {
         //println!("write: seg(id0: {}, id1: {}, max_size: {}, ino: {}, offset: {})", id0, id1, seg_max_size, ino, offset);
         loop {
             //println!("write: seg(id0: {}, id1: {}, max_size: {})", id0, id1, seg_max_size);
-            let worker = self.io_pool.get_worker(id0, id1);
+            let worker = self.disk_io_pool.get_thread(id0, id1);
             let seg_dir = self.segment_mgr.get_segment_dir(id0, id1);
-            let (tx, mut rx) = mpsc::channel::<MsgFileWriteResp>(1);
+            let (tx, rx) = bounded::<MsgFileWriteResp>(1);
             let msg = MsgFileWriteOp{
                 id0: id0,
                 id1: id1,
@@ -188,69 +195,74 @@ impl Leader for LeaderLocal {
                 data: data.to_vec(),
                 resp_sender: tx,
             };
-            let ret = worker.send_disk_io(MsgFileOp::OpWrite(msg));
+            let ret = worker.do_io(MsgFileOp::OpWrite(msg));
             if !ret.is_success() {
                 println!("write: failed to send_disk_io for ino: {}, seg(id0: {}, id1: {}), err: {:?}",
                 ino, id0, id1, ret);
                 return Err(Errno::Eintr);
             }
-            let ret = self.exec.get_runtime().block_on(rx.recv());
-            if let Some(r) = ret {
-                if !r.err.is_success() {
-                    if r.err.is_enospc() {
-                        println!("write: segment(id0: {}, id1: {}, dir: {}) has no space left for ino: {} with offset: {}",
-                        id0, id1, seg_dir, ino, offset);
-                        let seg = self.segment_mgr.new_segment(&String::from(""));
-                        self.handle_mgr.add_segment(ino, &seg);
-                        id0 = seg.seg_id0;
-                        id1 = seg.seg_id1;
-                        seg_max_size = seg.max_size;
-                        println!("write: add new segment(id0: {}, id1: {}) for ino: {} with offset: {}",
-                    id0, id1, ino, offset);
-                        continue;
+            let ret = rx.recv();
+            match ret {
+                Ok(r) => {
+                    if !r.err.is_success() {
+                        if r.err.is_enospc() {
+                            println!("write: segment(id0: {}, id1: {}, dir: {}) has no space left for ino: {} with offset: {}",
+                            id0, id1, seg_dir, ino, offset);
+                            let seg = self.segment_mgr.new_segment(&String::from(""));
+                            self.handle_mgr.add_segment(ino, &seg);
+                            id0 = seg.seg_id0;
+                            id1 = seg.seg_id1;
+                            seg_max_size = seg.max_size;
+                            println!("write: add new segment(id0: {}, id1: {}) for ino: {} with offset: {}",
+                        id0, id1, ino, offset);
+                            continue;
+                        }
+                        println!("write: failed to write segment(id0: {}, id1: {}) for ino: {} with offset: {}, err: {:?}",
+                        id0, id1, ino, offset, r.err);
+                        return Err(r.err);
                     }
-                    println!("write: failed to write segment(id0: {}, id1: {}) for ino: {} with offset: {}, err: {:?}",
-                    id0, id1, ino, offset, r.err);
-                    return Err(r.err);
-                }
-                // write block success.
-                let b = Block {
-                    ino: ino,
-                    generation: 0,
-                    offset: offset,
-                    seg_start_addr: r.offset,
-                    seg_end_addr: r.offset + r.nwrite as u64,
-                    size: r.nwrite as i64,
-                };
-                let ret = self.handle_mgr.add_block(ino, id0, id1, &b);
-                if !ret.is_success() {
-                    println!("write: failed to add_block{:?} for ino: {} with offset: {}, err: {:?}", b, ino, offset, ret);
-                    return Err(ret);
-                }
-                
-                return Ok(BlockIo{
-                    id0: id0,
-                    id1: id1,
-                    offset: r.offset,
-                    size: r.nwrite,
-                });
-                // currently, will update the segments in close api.
-                // upload the block to meta server.
-                /*let ret = self.segment_mgr.upload_block(ino, id0, id1, &b);
-                if ret.is_success(){
+                    // write block success.
+                    let b = Block {
+                        ino: ino,
+                        generation: 0,
+                        offset: offset,
+                        seg_start_addr: r.offset,
+                        seg_end_addr: r.offset + r.nwrite as u64,
+                        size: r.nwrite as i64,
+                    };
+                    let ret = self.handle_mgr.add_block(ino, id0, id1, &b);
+                    if !ret.is_success() {
+                        println!("write: failed to add_block{:?} for ino: {} with offset: {}, err: {:?}", b, ino, offset, ret);
+                        return Err(ret);
+                    }
+                    
                     return Ok(BlockIo{
                         id0: id0,
                         id1: id1,
                         offset: r.offset,
                         size: r.nwrite,
                     });
+                    // currently, will update the segments in close api.
+                    // upload the block to meta server.
+                    /*let ret = self.segment_mgr.upload_block(ino, id0, id1, &b);
+                    if ret.is_success(){
+                        return Ok(BlockIo{
+                            id0: id0,
+                            id1: id1,
+                            offset: r.offset,
+                            size: r.nwrite,
+                        });
+                    }
+                    println!("write: failed to upload block{:?} for ino: {}, err: {:?}", b, ino, ret);
+                    return Err(ret);*/
                 }
-                println!("write: failed to upload block{:?} for ino: {}, err: {:?}", b, ino, ret);
-                return Err(ret);*/
+                Err(err) => {
+                    println!("write: failed to get response for seg(id0: {}, id1: {}) of ino: {} with offset: {}, err: {}", 
+                        id0, id1, ino, offset, err);
+                    return Err(Errno::Eintr);
+                }
             }
-            println!("write: got invalid response for seg(id0: {}, id1: {}) of ino: {} with offset: {}", 
-            id0, id1, ino, offset);
-            return Err(Errno::Eintr);
+            
         }
     }
 
@@ -283,12 +295,12 @@ impl Leader for LeaderLocal {
                 ino, s.seg_id0, s.seg_id1, b.offset, b.size);
             }
             //close the segment.
-            let worker = self.io_pool.get_worker(s.seg_id0, s.seg_id1);
+            let worker = self.disk_io_pool.get_thread(s.seg_id0, s.seg_id1);
             let msg = MsgFileCloseOp{
                 id0: s.seg_id0,
                 id1: s.seg_id1,
             };
-            let ret = worker.send_disk_io(MsgFileOp::OpClose(msg));
+            let ret = worker.do_io(MsgFileOp::OpClose(msg));
             if !ret.is_success(){
                 println!("failed to close seg: id0: {}, id1: {} for ino: {}, err: {:?}", 
                     s.seg_id0, s.seg_id1, ino, ret);
@@ -309,7 +321,8 @@ impl LeaderLocal {
     pub fn new(machine: &String, thr_num: u32, exec: &Executor, mgr: Rc<SegmentMgr>) -> Self {
         LeaderLocal {
             machine: machine.clone(),
-            io_pool: DiskIoThreadPool::new(thr_num, exec),
+            disk_io_pool: IoThreadPool::new(thr_num, &String::from("Disk"), exec, 
+            &DiskIoWorkerFactory::new()),
             exec: exec.clone(),
             segment_mgr: mgr,
             handle_mgr: FileHandleMgr::create(),
