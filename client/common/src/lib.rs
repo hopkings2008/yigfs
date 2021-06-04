4c53805ed5317af189fd595f843fe030ea9ac26f
duanjh7 <18204311659@163.com>
2021-06-04 13:53:08 +0800

    modify error to println before init log
    Signed-off-by: duanjh7 <18204311659@163.com>

--- client/common/src/lib.rs
+++ client/common/src/lib.rs
@@ -8,18 +8,17 @@ pub mod defer;
 pub mod runtime;
 pub mod thread;
 pub mod numbers;
-use log::{info, error};
 
 pub fn parse_config(path: String)->Result<config::Config, Box<dyn std::error::Error>>{
     let results = std::fs::read_to_string(path);
     match results {
         Ok(content) => {
             let cfg = toml::from_str(content.as_str())?;
-            info!("cfg: {:?}", cfg);
+            println!("cfg: {:?}", cfg);
             return Ok(cfg);
         }
         Err(error) => {
-            error!("failed to parse, err: {:?}", error);
+            println!("failed to parse, err: {:?}", error);
             return Err(Box::new(error));
         }
     }
